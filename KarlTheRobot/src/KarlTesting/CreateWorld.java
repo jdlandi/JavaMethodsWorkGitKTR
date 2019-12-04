b9ada42d6a4800472ac7a89ede0fab1942ed65e8
jdlandi <jdlandi@cps.edu>
2019-12-04 14:24:25 -0600

    v1.2 Plus KTR

--- KarlTheRobot/src/KarlTesting/CreateWorld.java
+++ KarlTheRobot/src/KarlTesting/CreateWorld.java
@@ -1,4 +1,22 @@
 package KarlTesting;
-
+import java.util.Scanner;
+import kareltherobot.*;
 public class CreateWorld {
+    public static void main
+            (String[] args) {
+        World.setSize(10, 10);
+        World.readWorld("world1.txt");
+        World.setVisible(true);
+        //new WorldBuilder(true);
+        UrRobot amy
+                = new
+                UrRobot(5, 2,
+                Directions.East,
+                3);
+        amy.move();
+        amy.move();
+        amy.turnLeft();
+        amy.putBeeper();
+        amy.move(); // amy.pickBeeper();
+    }
 }
