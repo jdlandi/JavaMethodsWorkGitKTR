a942dfd97dd7203fea1155347c0672a426398f2c
Juan <jdlandi@cps.edu>
2019-12-16 15:27:14 -0600

    12-16-19 2.0

--- myStuff/src/Connect4/Piece.java
+++ myStuff/src/Connect4/Piece.java
@@ -1,10 +1,11 @@
 package Connect4;
 
-public class piece {
+public class Piece {
     int x;
     int y;
+    int[] pos = new int[]{x,y};
     char team;
-    public piece(int a, int b, char c) {
+    public Piece(int a, int b, char c) {
         this.team = c;
         this.x = a;
         this.y = b;
--- myStuff/src/Connect4/board.java
+++ myStuff/src/Connect4/board.java
@@ -1,7 +1,24 @@
 package Connect4;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class board {
-    piece[] pieces = new ArrayList<piece>().toArray(new piece[0]);
+    Piece[] pieces;
+    Piece[] set = new Piece[42];
+    public void init() {
+        for (int i = 1; i <= 6; i ++ ) {
+            for (int j = 1; j <= 7; j ++) {
+                set[((j - 1) + ((i - 1) * 7))] = new Piece(i,j,'-');
+            }
+        }
+    }
+    public void addPiece(Piece piece, char team) {
+        for (int i = 1; i <= 6; i ++ ) {
+            for (int j = 1; j <= 7; j ++) {
+                if (Arrays.equals( set[((j - 1) + ((i - 1) * 7))].pos, piece.pos)) {
+                    set[((j - 1) + ((i - 1) * 7))] = new Piece(piece.x,piece.y,team);
+                };
+            }
+        }
+    }
 }
